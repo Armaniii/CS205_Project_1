58e55770da81d032ec13412848c5f77d3af9f229
Arman Irani <airan002@ucr.edu>
2021-05-11 20:13:02 -0700

    Update 8-puzzle.py

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -18,7 +18,44 @@ class Node():
         self.locX = i%3
         self.locY = i/3
     
- 
+    def move(direction):
+        index = -1
+        if(direction == 0 and self.locY != 0):
+            index = (locY - 1) * numrows + locX
+        elif(direction == 1 and locY != numrows - 1):
+            index = (locY + 1) * numrows + locX
+        elif(direction == 2 and locX != 0):
+            index = locY * numrows + locX - 1
+        elif(direction == 3 and locX != numrows - 1)
+            index = locY*numrows + locX + 1
+        else
+            return -1
+        
+        state_update = self.state.copy()
+        temp = state_update[index]
+        state_update[index] = state_update[locY * numrows + locX]
+        state_update[locY * numrows + locX] = temp
+        return state_update
+
+class Problem():
+    def __init__(self, init_state, goal_state, algo):
+        self.init_state = int_state
+        self.goal_state = [1,2,3,4,5,6,7,8,0]
+        self.algo = algo
+        head = [(self, 0)]      
+
+    def solve():
+        repeated = set([])
+        limit = 0
+
+
+        while head:
+            for i in range(4):
+                head.state = move(i)
+
+
+
+
 if __name__ == "__main__":
     print("Welcome to the 8-Puzzle")
 
